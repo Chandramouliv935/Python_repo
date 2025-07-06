d185f04d432e2b882d5b189c73d7382b98ecf328
Chandramouliv935 <chandramouliv935@gmail.com>
2025-07-06 22:06:04 +0530

    files addded

--- mul_without_op.py
+++ mul_without_op.py
@@ -1,1 +1,29 @@
-print("Enter two numbers to multiply them without using '*' operator")
+n1 = int(input("enter 1:"))
+n2 = int(input("enter 2:"))
+product = 0
+count=n1
+while count>0:
+    count -= 1
+    product = product + n2
+print(f"Result: {n1} x {n2} = {product} without *")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
